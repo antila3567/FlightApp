c1d32c62cbb157663d858358ac81aa8068db8540
IvanL <antila3567@gmail.com>
2023-12-22 15:28:04 +0200

    Initial Commit

---
+++ flight/flightApp.swift
@@ -0,0 +1,17 @@
+//
+//  flightApp.swift
+//  flight
+//
+//  Created by Иван Легенький on 22.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct flightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
